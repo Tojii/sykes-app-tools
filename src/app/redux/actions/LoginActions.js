55a7481781bf7e5668250a5f57641b81c75d656c
AlejandroSibaja <ale9713@gmail.com>
2021-02-18 17:52:33 -0600

    fix login

--- src/app/redux/actions/LoginActions.js
+++ src/app/redux/actions/LoginActions.js
@@ -2,6 +2,8 @@ import apiAuthService from "../../services/apiAuthService";
 import { setUserData } from "./UserActions";
 import history from "history.js";
 import configureStore from "../Store";
+import axios from "axios";
+import jwtDecode from 'jwt-decode';
 
 export const LOGIN_ERROR = "LOGIN_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
@@ -16,31 +18,42 @@ export function loginWithEmailAndPassword({ email, password }) {
       type: LOGIN_LOADING
     });
 
-    apiAuthService
-      .loginWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch(setUserData(user));
-        //console.log("history",history)
-        if (history.location.prev) {
-          history.push({
-            pathname: history.location.pathname != history.location.prev ? history.location.prev : "/"
-          });
-        } else {
-          history.push({
-            pathname: (history.state && history.state != "/session/signin") ? history.state :  "/"
-          });
-        }
+    const parameters = {
+      username: email,
+      password: password,
+      force: true
+    }
+    axios.defaults.headers.common["x-api-key"] = `${process.env.REACT_APP_X_API_KEY}`;
+    return axios.post(`${process.env.REACT_APP_API_URL}/authenticate`, parameters).then(response => {
+      // Login successful
+      // Save token
+      //console.log("Login", response);
+      apiAuthService.setSession(response.data.token);
+      // Set user
+      apiAuthService.setUser(response.data.token);
+      dispatch(setUserData(jwtDecode(response.data.token)));
+      //console.log("history",history)
+      if (history.location.prev) {
+        history.push({
+          pathname: history.location.pathname != history.location.prev ? history.location.prev : "/"
+        });
+      } else {
+        history.push({
+          pathname: (history.state && history.state != "/session/signin") ? history.state :  "/"
+        });
+      }
 
-        return dispatch({
-          type: LOGIN_SUCCESS
-        });
-      })
-      .catch(error => {
-        return dispatch({
-          type: LOGIN_ERROR,
-          payload: error
-        });
+      return dispatch({
+        type: LOGIN_SUCCESS
       });
+      //return jwtDecode(response.data.token);
+    })
+    .catch(error => {
+      return dispatch({
+        type: LOGIN_ERROR,
+        payload: error
+      });
+    });
   };
 }
 
